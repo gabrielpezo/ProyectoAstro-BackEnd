23518e2c6a1588fcfc725a1c278a69fad4b10157
Ambrosio <ambrosio.rhc@gmail.com>
2024-05-15 19:24:49 -0400

    avanzando

--- app.py
+++ app.py
@@ -1,52 +1,87 @@
 from flask import Flask, jsonify, request
-from models import db, User
+from models import db, User, Photos
 from flask_migrate import Migrate
-
-
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///dataBase.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
 db.init_app(app)
 migrate = Migrate(app, db)
 
-@app.route('/get_users', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def handle_get_all_users():
-    handle_get_all_users = User().query.all()
-    get_all_users = list(map(lambda user:user.serialize(), handle_get_all_users))
-    return jsonify({"msg": "success", "users": get_all_users}), 200
+    users = User.query.all()
+    users_list = [user.serialize() for user in users]
+    return jsonify({"msg": "success", "users": users_list}), 200
 
-@app.route("/get_users/<int:id>", methods=["GET"])
+@app.route("/users/<int:id>", methods=["GET"])
 def handle_get_user(id):
-    get_user = User.query.get_or_404(id)  
-    return jsonify(get_user.serialize()), 200
+    user = User.query.get_or_404(id)
+    return jsonify(user.serialize()), 200
 
-@app.route("/create_user", methods=["POST"])
-def handle_user():
+@app.route("/users", methods=["POST"])
+def handle_create_user():
     data = request.get_json()
-    create_user = User()
-    create_user.name = data["name"]
+    new_user = User(name=data["name"], email=data["email"], password=data["password"])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"msg": "success", "user": new_user.serialize()}), 201
 
-    db.session.add (create_user)
-    db.session.commit() 
-    return jsonify({"msg": "success", "data":data}), 201
-
-@app.route("/get_users/<int:id>", methods=["PUT"])
+@app.route("/users/<int:id>", methods=["PUT"])
 def update_user(id):
     data = request.get_json()
-    update_user = User().query.filter_by(id=id).first()
-    if update_user is not None:
-        update_user.name = data["name"]
-        db.session.commit()
-        return jsonify({"msg": "user upgraded", "user": update_user.serialize()}), 200
-    else:
-        return jsonify({"msg": "user not found"}), 404
+    user = User.query.get_or_404(id)
+    user.name = data.get("name", user.name)
+    user.email = data.get("email", user.email)
+    user.password = data.get("password", user.password)
+    db.session.commit()
+    return jsonify({"msg": "user updated", "user": user.serialize()}), 200
 
-@app.route('/get_users/<int:id>', methods=['DELETE'])
+@app.route('/users/<int:id>', methods=['DELETE'])
 def delete_user(id):
-    get_user = User.query.get_or_404(id)
-    db.session.delete(get_user)
+    user = User.query.get_or_404(id)
+    db.session.delete(user)
     db.session.commit()
     return jsonify({"msg": "User was deleted"}), 200
 
+@app.route('/photos', methods=['GET'])
+def handle_get_all_photos():
+    photos = Photos.query.all()
+    photos_list = [photo.serialize() for photo in photos]
+    return jsonify({"msg": "success", "photos": photos_list}), 200
+
+@app.route('/photos/<int:id>', methods=['GET'])
+def handle_get_photo(id):
+    photo = Photos.query.get_or_404(id)
+    return jsonify(photo.serialize()), 200
+
+@app.route('/photos', methods=['POST'])
+def handle_create_photo():
+    data = request.get_json()
+    new_photo = Photos(id=data["id"], image=data["image"])
+    db.session.add(new_photo)
+    db.session.commit()
+    return jsonify({"msg": "success", "photo": new_photo.serialize()}), 201
+
+@app.route('/photos/<int:id>', methods=['PUT'])
+def handle_update_photo(id):
+    data = request.get_json()
+    photo = Photos.query.get_or_404(id)
+    photo.image = data.get("image", photo.image)
+    db.session.commit()
+    return jsonify({"msg": "photo updated", "photo": photo.serialize()}), 200
+
+@app.route('/photos/<int:id>', methods=['DELETE'])
+def handle_delete_photo(id):
+    photo = Photos.query.get_or_404(id)
+    db.session.delete(photo)
+    db.session.commit()
+    return jsonify({"msg": "Photo was deleted"}), 200
+
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  
     app.run(host="0.0.0.0", port="5000", debug=True)
+
+
+
--- models.py
+++ models.py
@@ -1,15 +1,141 @@
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'  # Cambia esto si usas otra base de datos
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 
-class User (db.Model):
+class User(db.Model):
     __tablename__ = "user"
-    id = db.Column (db.Integer, primary_key=True)
-    name = db.Column (db.String(100), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
     def serialize(self):
-        return{
+        return {
             "id": self.id,
-            "name": self.name
+            "name": self.name,
+            "email": self.email
+        }
+
+class Photos(db.Model):
+    __tablename__ = 'photos'
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String(100), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "image": self.image,
+        }
+
+class Photographer(db.Model):
+    __tablename__ = 'photographer'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    photos_id = db.Column(db.Integer, db.ForeignKey('photos.id'))
+    uploaded_photos = db.relationship("Photos")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "photos_id": self.photos_id
+        }
+
+class Favourites(db.Model):
+    __tablename__ = 'favourites'
+    id = db.Column(db.Integer, primary_key=True)
+    photographer_id = db.Column(db.Integer, db.ForeignKey('photographer.id'))
+    photographer = db.relationship("Photographer")
+    photo_id = db.Column(db.Integer, db.ForeignKey('photos.id'))
+    photo = db.relationship("Photos")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "photographer_id": self.photographer_id,
+            "photo_id": self.photo_id
+        }
+
+class FAQ(db.Model):
+    __tablename__ = 'faq'
+    id = db.Column(db.Integer, primary_key=True)
+    questions = db.Column(db.String(500), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship("User")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "questions": self.questions,
+            "user_id": self.user_id
+        }
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comments = db.Column(db.String(1000), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship("User")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "comments": self.comments,
+            "user_id": self.user_id
+        }
+
+class Cart(db.Model):
+    __tablename__ = 'cart'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(20), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship("User")
+    photo_id = db.Column(db.Integer, db.ForeignKey('photos.id'))
+    photo = db.relationship("Photos")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "date": self.date,
+            "user_id": self.user_id,
+            "photo_id": self.photo_id
+        }
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    photo_id = db.Column(db.Integer, db.ForeignKey('photos.id'))
+    photo = db.relationship("Photos")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "photo_id": self.photo_id
+        }
+
+class Followers(db.Model):
+    __tablename__ = "followers"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship("User")
+    photographer_id = db.Column(db.Integer, db.ForeignKey('photographer.id'))
+    photographer = db.relationship("Photographer")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "photographer_id": self.photographer_id
         }
